40a433c3553ce8edc177e7cceab7afdc19bdbb0e
Varun Kandhari <75046190+Varun612@users.noreply.github.com>
2025-05-17 04:42:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const el = document.getElementById('level');
+let level=0;
+let currEl=el;
+while(currEl) {
+	level++;
+	currEl = currEl.parentElement
+}
+alert(`The level of the element is: ${level}`);
